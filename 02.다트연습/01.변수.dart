3f8c5328d4c42afce94bfdc83c9b4cbb44072340
Tom B. Parker <tombap8@hotmail.com>
2023-12-15 09:26:55 +0900

    Update 01.변수.dart

--- 02.다트연습/01.변수.dart
+++ 02.다트연습/01.변수.dart
@@ -1,4 +1,8 @@
 void main(List<String> args) {
+
+  // 다트언어는 변수형을 자동으로 
+  // 인식하는 선언이 있음 -> 타입추론
+  // 1. var
   var myHome = "내집이야~!";
   print(myHome);
   // var는 처음에 할당한 값의 형으로 재할당해야함!
@@ -6,6 +10,19 @@ void main(List<String> args) {
   myHome = "너의집도 크다!";
   print(myHome);
 
+  // 2. dynamic 
+  // -> var와 비슷하지만 나중에 재할당시 
+  // 처음 할당한 값과 다른형의 값을 넣어도 괜찮다!
+  dynamic myId = "hhh2345";
+
+  print("나의 아이디는 $myId");
+
+  myId = 787878;
+
+  print("너의 아이디는 $myId");
+
+
+  // [ 문자형 출력방법 : '${코드}' 또는 '$변수' ]
 
 
   // 다트언어는 변수의 형을 선언함이 기본이다!
@@ -20,7 +37,7 @@ void main(List<String> args) {
   // 숫자형도 정수형과 실수형을 철저히 구분함!
   double number2 = 7;
   number2 = 3.14;
-  print('원주율값은 '+number2.toString());
+  print(number2);
 
   num number3 = 100;
   number3 = 7.84;
@@ -29,11 +46,11 @@ void main(List<String> args) {
   // 2. 문자형 : String
   String name = "톰 행크스";
   // name = 88;
-  print("나는 "+name+"입니다!");
+  print("나는 $name입니다!");
 
   // 3. 불린형 : bool
   bool isTrue = true;
-  print(isTrue);
+  print("진짜인가? 가짜인가? $isTrue");
 
   // 4. 콜렉션 : 묶음데이터
   // List / Set / Map
@@ -42,26 +59,55 @@ void main(List<String> args) {
   print(we[2]+'는 모두 친구입니다!');
   print(we.length);
 
-  // 4-2. Set형 - 순서가 없고 중복되지 않는 데이터집합
-  Set<int> evens = {2,4,6,8,10};
+  // 4-2. Set형 - 
+  // ->순서가 없고 중복되지 않는 데이터집합
+  // ->순서가 없으므로 호출해서 사용코자 할때는 List형으로 변환
+  // 후 사용한다! -> Set형변수.toList()
+
+
+  // Set<int> evens = {2,4,6,8,10,4};
+  // Set<dynamic> evens = {2,4,6,8,10,4,'짝수'};
+  var evens = {2,4,6,8,10,4,'짝수'};
   print(evens);
+  // 리스트형으로 변환
+  print(evens.toList());
+  // 변환후에는 순번으로 출력가능!
+  print(evens.toList()[3]);
 
   // 4-3. Map형 - 데이터에 라벨링된 데이터 집합(객체와 유사)
   Map<String,String> actor = {"이름":"강동원","나이":"40"};
   print(actor);
 
-  // [형변환(Casting)] : 특정형에서 다른 형으로 변환하는 것(캐스팅이라고도 함)
-  // 일반적인 형변환 : 변수형.parse(대상변수) 
-  // -> 특히 숫자일때는 작은것에서 큰것으로 변환함
-  // 문자형 형변환 메서드 : 변수.toString()
+
+  // [ 다트의 형변환 (Casting) ]
+  // -> 특정 데이터형에서 다른 형으로 변환하는것을
+  // 흔히 캐스팅이라고도 함!
+
+  // 1. 일반적인 형변환 : 변수형.parse(대상변수)
+  // 2. 메서드 체인방식 : 변수.to형()
+  // -> 변수.toString() -> 문자형변환
+  // -> 변수.toList() -> 컬랙션 List형변환
+
+  // 주의: 형변환이 안될것 같으면 변환하지 마라!
+  // -> 예컨데 문자형 문자를 숫자로 변경못함!
   String stNum = "777";
-  print("문자형 숫자:"+stNum);
+  print('문자형 숫자: $stNum');
 
+  // 문자형숫자를 캐스팅하여 숫자로계산하기
   num result = 111 + int.parse(stNum);
-  // 숫자로 형변환
-  print(stNum+"에 111을 더한 결과는?"+result.toString());
+  print("111 + 777 = $result");
 
+  String movie(num snum){
+    var temp = free
+    return temp;
+  }
 
+  // 자유 출력
+  var free = ['노량','한산','명량'];
+
+  print("이순신 최후의 전투는 ${free[2]}해전이다!");
+
+  print('이순신 영화 3가지는 ${} 이다!');
 
 
 }

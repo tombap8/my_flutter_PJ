462be1b4bed98e5984cb988f5fe97ddfdda1cf6b
Tom B. Parker <tombap8@hotmail.com>
2023-12-25 23:31:42 +0900

    ss

--- 03.플러터PJ/app_01_store/lib/main.dart
+++ 03.플러터PJ/app_01_store/lib/main.dart
@@ -1,48 +1,78 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 // stl로 쓰면 자동완성됨!
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: StorePage(), // 이 클래스를 아래에 만듦
     );
   }
 }
 
 class StorePage extends StatelessWidget {
-  const StorePage({super.key});
+  StorePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: SafeArea(
       child: Column(
         children: [
-          Row(children: [
-            Text(
-              "Woman",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Kids",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Shoes",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Bag",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ])
+          // Padding 위젯
+          /*             
+            padding: EdgeInsets.all(4방향전체),
+            padding: EdgeInsets.only(left/top/right/bottom특정방향만),
+            padding: EdgeInsets.symmetric(수직/수평중),
+           */
+          Padding(
+            padding: EdgeInsets.all(25.0),
+            child: Row(children: [
+              Text(
+                "Woman",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+              Spacer(),
+              Text(
+                "Kids",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                  fontSize: 20.0,
+                ),
+              ),
+              Spacer(),
+              Text(
+                "Shoes",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+              Spacer(),
+              Text("Bag",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                    fontSize: 20.0,
+                  )),
+            ]),
+          ),
+          // 이미지 추가하기
+          Image.asset(
+            "assets/bag.jpeg",
+            fit: BoxFit.cover,
+          ),
         ],
       ),
     ));

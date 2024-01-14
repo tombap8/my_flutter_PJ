aa7cf15969e05a36bed9fe4c131ff679505d8b20
Tom B. Parker <tombap8@hotmail.com>
2024-01-14 21:10:22 +0900

    ss

---
+++ 03.플러터PJ/app_06_airbnb/lib/components/home/home_header.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:app_06_airbnb/components/home/home_header_appbar.dart';
+import 'package:app_06_airbnb/components/home/home_header_form.dart';
+import 'package:app_06_airbnb/size.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: header_height,
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/background.jpeg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            HomeHeaderAppBar(),
+            HomeHeaderForm(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ 03.플러터PJ/app_07_carrot/android/app/src/main/kotlin/com/example/app_07_carrot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_07_carrot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
